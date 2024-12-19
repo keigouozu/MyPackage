88027dcc0f59fe85d49d066e3c70773de3e15aa0
Keigo Uozu <s23c1021jf@s.chibakoudai.jp>
2024-12-19 23:55:07 +0900

    cpu_usage.pyを追加

---
+++ mypkg/mypkg/cpu_usage.py
@@ -0,0 +1,28 @@
+import rclpy
+import psutil
+from rclpy.node import Node
+from std_msgs.msg import Float32  
+
+
+class CpuUsagePublisher(Node):
+    def __init__(self):
+        super().__init__("cpu_usage_publisher")
+        self.publisher_ = self.create_publisher(Float32, "cpu_usage", 10)
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        cpu_usage = psutil.cpu_percent(interval=None)  
+        msg = Float32()
+        msg.data = cpu_usage
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Publishing CPU usage: {cpu_usage}%")
+
+
+def main():
+    rclpy.init()
+    node = CpuUsagePublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
