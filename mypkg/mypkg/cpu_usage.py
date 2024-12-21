7ca360190de34eb22218595daf36f5d28757e6cb
Keigo Uozu <s23c1021jf@s.chibakoudai.jp>
2024-12-21 20:40:25 +0900

    著作権とライセンスを追加

--- mypkg/mypkg/cpu_usage.py
+++ mypkg/mypkg/cpu_usage.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Keigo Uozu
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 import psutil
 from rclpy.node import Node
@@ -7,16 +10,16 @@ from std_msgs.msg import Float32
 class CpuUsagePublisher(Node):
     def __init__(self):
         super().__init__("cpu_usage_publisher")
-        self.publisher_ = self.create_publisher(Float32, "cpu_usage", 10)
+        self.pub = self.create_publisher(Float32, "cpu_usage", 10)
         psutil.cpu_percent(interval=1.0)
-        self.create_timer(1.0, self.timer_callback)
+        self.create_timer(1.0, self.cb)
 
-    def timer_callback(self):
+    def cb(self):
         cpu_usage = psutil.cpu_percent(interval=None)  
         msg = Float32()
         msg.data = cpu_usage
-        self.publisher_.publish(msg)
-        self.get_logger().info(f"Publishing CPU usage: {cpu_usage}%")
+        self.pub.publish(msg)
+        self.get_logger().info(f"CPU usage: {cpu_usage}%")
 
 
 def main():
@@ -24,6 +27,3 @@ def main():
     node = CpuUsagePublisher()
     rclpy.spin(node)
     rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
